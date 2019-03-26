12ffd7f7ab030978fc48ff97037ae9fd9aebc1ae
skwas <s.kwasnicki@gmail.com>
2019-03-26 22:23:12 +0100

    initial commit

---
+++ src/main/java/com/sk/web/controllers/BasicController.java
@@ -0,0 +1,24 @@
+package com.sk.web.controllers;
+
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", "dzien dobry");
+
+        return "main";
+    }
+
+
+    @RequestMapping("/siemanko")
+    public String index() {
+        return "Greetings test!";
+    }
+}
